3f029764c2057d0adb39c7af3d459fea058fe51a
Onyeabor Joel <earlmorningstar@gmail.com>
2023-06-14 15:47:09 +0100

    Update 1

---
+++ script.js
@@ -0,0 +1,77 @@
+// console.log("Hello From Me To JavaScript");
+// let age = 1985;
+
+// if (1 + 1 === 2){
+//     console.log("True as norms")
+// }
+
+// if (2 + 2 ===6){
+//     console.log("Never Display")
+// }
+
+// let number = 10;
+
+// if (number > 11) {
+//     console.log("The Number Is Good To GO!");
+// } else {
+//     console.log("The Number Isn't Good To Go!")
+// }
+
+// const dayOfTheWeek = 'Friday';
+
+// if (dayOfTheWeek === 'Monday') {
+//     console.log("Nah, Not Today, Fuck!!")
+// } else if (dayOfTheWeek === 'Friday') {
+//     console.log("Great Day To Be Alive")
+// }
+
+// Example Using const, if and else if;
+
+// 0 - 5 - free
+// 5 - 10 Child $10
+// 10 - 65 Adult $20
+// 65+ Senior $10
+
+// const age = 120;
+
+// if (age < 5) {
+//     console.log("You are a baby. You get in for free.")
+// } else if (age < 10) {
+//     console.log ("You are a child. Your bill is $10.")
+// } else if (age < 65) {
+//     console.log ("You are an adult. Your biil is $20.")
+// } else {
+//     console.log ("You are a senior. Your bill is $10.")
+// }
+
+// let getColor = phrase;
+
+//   if (phrase === 'stop') {
+//     console.log ("red");
+// } else if (phrase === 'slow') {
+//     console.log ("yellow");
+// } else if (phrase ==== 'go') {
+//     console.log ("green");
+// } else {
+//     console.log ("purple");
+// }
+
+const password = prompt("Please, create a new password");
+
+if (password.length >= 8) {
+    if(password.indexOf(' ') === -1){
+        console.log("Valid Password!!")
+    } else {
+        console.log("Password Cannot Contain Spaces!")
+    }
+} else {
+    console.log("Passsword too short! Must contain 6+ charaters")
+}
+
+
+
+{
+  console.log ("Valid Password!!");
+} else {
+  console.log ("Passsword too short! Must contain 6+ charaters");
+}
