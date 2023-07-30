6a748bd7a957fbe288d3f48b195f0aa5cbbabc56
Onyeabor Joel <earlmorningstar@gmail.com>
2023-07-30 16:01:28 +0100

    Event-Listener

--- script.js
+++ script.js
@@ -890,3 +890,20 @@
 // setTimeout(() => {
 //     console.log("...Are you still there?")
 // }, 3000)
+
+// ADD EVENT LISTENER  .addEventListener()
+
+const button = document.querySelector("button");
+const h1 = document.querySelector("h1");
+button.addEventListener("click", () => {
+  const newColor = randomColor();
+  document.body.style.backgroundColor = newColor;
+  h1.textContent = newColor;
+});
+
+const randomColor = () => {
+  const r = Math.floor(Math.random() * 255);
+  const g = Math.floor(Math.random() * 255);
+  const b = Math.floor(Math.random() * 255);
+  return `rgb(${r}, ${g}, ${b})`;
+};
