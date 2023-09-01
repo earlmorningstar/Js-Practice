66821bb5b0ba19a98bbf86c5d867cdfb9a45ea3d
Onyeabor Joel <earlmorningstar@gmail.com>
2023-09-01 15:42:15 +0100

    Updated Script 24.
    PROTOTYPES - learning how it works.

--- script.js
+++ script.js
@@ -1045,24 +1045,36 @@
 
 // <!-- ---Tv Show Search--- -->
 
-const form = document.querySelector("#searchForm");
+// const form = document.querySelector("#searchForm");
 
-form.addEventListener("submit", async function (e) {
-  e.preventDefault();
-  const searchTerm = form.elements.query.value;
-  const config = { params: { q: searchTerm } }; //You can add other params. Eg: Joel: 'is funny'
-//   const configTwo = {headers: {accept: 'application/json'}};
-  const res = await axios.get(`https://api.tvmaze.com/search/shows`, config);
-  makeImages(res.data);
-  form.elements.query.value = "";
-});
+// form.addEventListener("submit", async function (e) {
+//   e.preventDefault();
+//   const searchTerm = form.elements.query.value;
+//   const config = { params: { q: searchTerm } }; //You can add other params. Eg: Joel: 'is funny'
+// //   const configTwo = {headers: {accept: 'application/json'}};
+//   const res = await axios.get(`https://api.tvmaze.com/search/shows`, config);
+//   makeImages(res.data);
+//   form.elements.query.value = "";
+// });
 
-const makeImages = (shows) => {
-  for (let result of shows) {
-    if (result.show.image) {
-      const img = document.createElement("IMG");
-      img.src = result.show.image.medium;
-      document.body.append(img);
-    }
-  }
+// const makeImages = (shows) => {
+//   for (let result of shows) {
+//     if (result.show.image) {
+//       const img = document.createElement("IMG");
+//       img.src = result.show.image.medium;
+//       document.body.append(img);
+//     }
+//   }
+// };
+
+// <!-- ---Tv Show Search Ends--- -->
+
+
+// ----PROTOTYPES - How it works----
+
+String.prototype.yell = function() {
+    return `OMG!!! ${this.toUpperCase()}!!!! ARRRRGGHH!!`;
 };
+
+console.log('i love you'.yell());  // OMG!!! I LOVE YOU!!!! ARRRRGGHH!!
+
